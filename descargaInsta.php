f7e1058357c4e9f5353a69497b3646ef0318371f
Jorge Martínez Mauricio <jorgem@gmail.com>
2016-08-22 12:49:37 -0500

    extractor de ligas instagram

--- descargaInsta.php
+++ descargaInsta.php
@@ -6,6 +6,7 @@
    * @ToRo 2016
    */
    $url = (isset($argv[1]) && preg_match("/(instagram|\.txt)/i",$argv[1])) ? $argv[1] : null;
+   $tamano=0;
    if(empty($url)) {
       die("\nUso: php $argv[0] FOTO_INSTAGRAM | TXT\n\n");
    }
@@ -13,6 +14,7 @@
       if(!is_file($url)) die("No existe una lista de direcciones en $url :(\n\n");
       $file = file($url);
       if(empty($file)) die("no hay nada en el archivo $url\n");
+      $tamano = sizeof($file);
       foreach($file AS $url) {
          if(preg_match("/instagram/",$url)) {
             instagram($url);
@@ -21,14 +23,17 @@
    } else {
       instagram($url);
    }
-
    function instagram($url) {
       $url = trim($url);
-      echo "Descargando: $url ...";
+      echo "* ".$url;
       //
       $agent= "Mozilla/5.0 (Windows NT 6.0; rv:16.0) Firefox/13.0"; //user agent
-      $context = stream_context_create(['http'=>['user_agent'=> $agent ]]);
-      $data = file_get_contents($url,false,$context);
+      $context = stream_context_create(['http'=>['user_agent'=> $agent, 'referer'=> 'https://www.instagram.com/' ]]);
+      @$data = file_get_contents($url,false,$context);
+      if(empty($data)) {
+         echo " -- parece que se pudo obtener la información\n";
+         return;
+      }
       //
       preg_match("/\"owner\": {\"username\": \"(.*)\",\s\"is_unpublished\"/",$data,$y);
       $username = null;
@@ -38,19 +43,21 @@
          $username = trim($y[1]);
       }
       $path=(!empty($username))? "instagram/".$username."/": "instagram/";
-      echo $username;
+      echo " = ".$username;
       if(!empty($path) && !is_dir($path)) mkdir($path,0755,true);
       //buscamos video
       preg_match("/property=\"og:video\" content=\"(.*)\"/",$data,$y);
+      $touch=0;
       if(isset($y[1]) && preg_match("/^http/",$y[1])) {
          $imagen = $y[1];
          preg_match("/(.*)\/(.*)\.mp4/i",$imagen,$v);
          $name = empty($v[2]) ? uniqid() : $v[2];
-         echo "! => video ".$name.".mp4 ";
+         echo "!";
          if(!is_file($path.$name.".mp4")) {
             file_put_contents($path.$name.".mp4", file_get_contents($imagen,false,$context));
+            $touch++;
+            echo " => MP4 ".$name.".mp4 new";
          }
-         echo " ok!\n";
       } else {
          //buscamos imagen
          preg_match("/property=\"og:image\" content=\"(.*)\"/",$data,$y);
@@ -58,19 +65,24 @@
             $imagen = $y[1];
             preg_match("/(.*)\/(.*)\.jpg/i",$imagen,$v);
             $name = empty($v[2]) ? uniqid() : $v[2];
-            echo "! => imagen ".$name.".jpg ";
+            echo "!";
             if(!is_file($path.$name.".jpg")) {
                file_put_contents($path.$name.".jpg", file_get_contents($imagen,false,$context));
+               $touch++;
+               echo " => JPG ".$name.".jpg new";
             }
-            echo " ok!\n";
          }
       }
       //meta
-      $meta="https://tar.mx/log/descargar-imagenes-de-instagram-con-php/\n";
-      foreach(['url','title'] AS $k) {
-         preg_match("/property=\"og:".$k."\" content=\"(.*)\"/",$data,$y);
-         if(!empty($y[1])) $meta .= strtoupper($k).": ".$y[1]."\n";
+      if($touch>0) {
+         $meta="https://tar.mx/log/descargar-imagenes-de-instagram-con-php/ ".strftime("%c")."\nUSER: $username\n";
+         foreach(['url','title'] AS $k) {
+            preg_match("/property=\"og:".$k."\" content=\"(.*)\"/",$data,$y);
+            if(!empty($y[1])) $meta .= strtoupper($k).": ".$y[1]."\n";
+         }
+         file_put_contents($path.$name.".txt",$meta);
       }
-      file_put_contents($path.$name.".txt",$meta);
+      sleep(1);
+      echo " ok!\n";
    }
 ?>
---
+++ extractURL.php
@@ -0,0 +1,23 @@
+<?php
+   /*
+   * extrae los url de un perfil de instagram, es manual
+   * 1. recorrer el perfil del usuario para que aparezcan las imagenes (thumbnails)
+   * 2. recorrer recorrer recorrer
+   * 3. seguir recorriendo hasta donde quieras
+   * 4. en inspeccionar elemento navega hasta body -> span -> seccion -> article -> div ... 
+   * es el contenedor de cada fila de fotos, copiar ese contenido (botón derecho, Copy , Copy outerHTML)
+   * 5. pegar ese contenido en un archivo de texto. Ejecutar este script tomando como argumento tal archivo
+   * digamos extractURL.php temporal.txt > lista.txt -- esto va a generar un listado  en el 
+   * archivo "lista.txt"
+   * @toro 2016
+   */
+   if(!isset($argv[1]) || !is_file($argv[1])) die("\nUso: ".$argv[0]." temporal.txt > lista.txt\n\n");
+   //
+   $data= file_get_contents($argv[1]);
+   preg_match_all("/href=\"\/p\/([A-Za-z0-9_\-]{1,})+\/\?taken/",$data,$y);
+   if(!empty($y[1])) {
+      foreach($y[1] AS $k) {
+         echo "https://www.instagram.com/p/".$k."/\n";
+      }
+   }
+?>
